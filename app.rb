d6fa157e82466e270f490519552d2564aa336ed3
Austen Grice-Griffin <austen.grice-griffin@Austens-MacBook-Pro.local>
2018-11-12 15:21:19 +0000

    sign up db

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require 'pg'
 
 class MakersBnB < Sinatra::Base
   enable :sessions, :method_override
@@ -6,4 +7,18 @@ class MakersBnB < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  get '/signup' do
+    erb :sign_up
+  end
+
+  post '/' do
+    username = params['username']
+    password = params['password']
+    connection = PG.connect(dbname: 'posts')
+    connection.exec("INSERT INTO user_details (username, password) VALUES('#{username}', '#{password}')")
+
+    redirect '/'
+  end
+
 end
